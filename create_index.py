6084eccf7ece0b8731c656a22f18492ba74467b9
hgarud <hrishi.garud@gmail.com>
2024-01-17 22:27:50 -0800

    add abstract summary creation and embedding

--- create_index.py
+++ create_index.py
@@ -23,6 +23,7 @@ def main():
     openai_client = OpenAI()
     pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment="us-east1-gcp")
     index = get_pinecone_index('papers', 1536, 'cosine', 'p1')
+    summarize_prompt = "Please summarize the given abstract of an AI research paper so that a search engine can retrieve relevant papers based on cosine similarity between vector embeddings of the summary and input user query. Be extremely frugal with output words. Do not include any results. Abstract:"
 
     # Create embeddings for all papers in the dataset
     query_cats = set(['cs.AI', 'cs.CL', 'cs.CV', 'cs.LG', 'cs.NE', 'cs.RO'])
@@ -38,7 +39,18 @@ def main():
                 abstract = data['abstract'].strip().replace('\n', ' ')
                 text = title + ' ' + abstract
                 response = openai_client.embeddings.create(input = text, model='text-embedding-ada-002')
-                embeddings[pid] = response.data[0].embedding
+                embeddings[pid] = {'main': response.data[0].embedding}
+                response = openai_client.chat.completions.create(
+                                model="gpt-4",
+                                messages=[
+                                    {"role": "system", "content": summarize_prompt},
+                                    {"role": "user", "content": abstract}
+                                ]
+                            )
+                summary =  response.choices[0].message.content
+                response = openai_client.embeddings.create(input = summary, model='text-embedding-ada-002')
+                summary_embedding = response.data[0].embedding
+                embeddings[pid]['summary'] = summary_embedding
     
     # Add embeddings to Pinecone index
     for pid, embedding in embeddings.items():
