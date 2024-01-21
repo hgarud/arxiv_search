316bb9fa866ba8db9ae477d165f2c7feb3ee540f
hgarud <hrishi.garud@gmail.com>
2024-01-21 15:28:22 -0800

    add main script

---
+++ main.py
@@ -0,0 +1,38 @@
+"""Main API script for the AI Research Paper Search Engine."""
+
+import argparse
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from openai import OpenAI
+import pinecone
+
+
+def main(query: str):
+    # Setup
+    openai_client = OpenAI()
+    pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment="us-east1-gcp")
+    index = pinecone.Index("papers")
+
+    # Create embeddings for query
+    response = openai_client.embeddings.create(input = query, model='text-embedding-ada-002')
+    query_embedding = response.data[0].embedding
+
+    # Search for similar papers
+    results = index.query(
+        vector=query_embedding,
+        top_k=5,
+        include_values=False,
+        namespace='main'
+    )
+    return [paper['id'] for paper in results['matches']]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='AI Research Paper Search Engine')
+    parser.add_argument('--query', type=str, help='Query string')
+
+    args = parser.parse_args()
+
+    print(main(args.query))
